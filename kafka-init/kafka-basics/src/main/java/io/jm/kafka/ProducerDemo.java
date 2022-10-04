4c865c647751ff0f7d017c0ed2dff0ab51028478
Jose Manzano G <j.manzano.g@hotmail.com>
2022-10-04 17:52:28 -0500

    Producer demo

--- kafka-init/kafka-basics/src/main/java/io/jm/kafka/ProducerDemo.java
+++ kafka-init/kafka-basics/src/main/java/io/jm/kafka/ProducerDemo.java
@@ -1,7 +1,39 @@
 package io.jm.kafka;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
     public static void main(String[] args) {
+        log.info("Hello kafka");
+
+        // configure kafka producer properties
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // instance a producer and a record  to sent to kafka
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("Demo_Java_Topic", "Key001", "Value001");
+
+            // send async
+            producer.send(record);
+
+            // wait to send all message
+            producer.flush();
+        }
+        log.info("end  kafka");
 
     }
 }
