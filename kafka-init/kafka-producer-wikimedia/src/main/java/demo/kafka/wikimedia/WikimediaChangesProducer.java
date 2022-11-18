3afdf07b9a5f03e88579bbffd67833c7392b3fea
Jose Manzano G <j.manzano.g@hotmail.com>
2022-11-18 13:01:44 -0500

    message compression active

--- kafka-init/kafka-producer-wikimedia/src/main/java/demo/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-init/kafka-producer-wikimedia/src/main/java/demo/kafka/wikimedia/WikimediaChangesProducer.java
@@ -12,7 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.BATCH_SIZE_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.COMPRESSION_TYPE_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class WikimediaChangesProducer {
@@ -24,6 +27,14 @@ public class WikimediaChangesProducer {
         properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // set high throughput producer configs
+        properties.setProperty(LINGER_MS_CONFIG, "20");
+        properties.setProperty(BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(COMPRESSION_TYPE_CONFIG, "snappy");
+
+
+
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
--- script/kafka-topics_sh_commands.md
+++ script/kafka-topics_sh_commands.md
@@ -9,3 +9,6 @@ kafka-topics.sh --bootstrap-server 127.0.0.1:9092 --describe --topic my_topic2 /
 kafka-topics.sh --bootstrap-server 127.0.0.1:9092 --delete --topic my_topic  //delete a topic
 
 kafka-run-class.sh kafka.tools.GetOffsetShell   --broker-list localhost:9092 --topic wikimedia.recent-change   | awk -F  ":" '{sum += $3} END {print "Result: "sum}' // check number of messages in the topic 
+
+kafka-log-dirs.sh --describe --bootstrap-server localhost:9092 --topic-list wikimedia.recent-change | grep '^{' \
+| jq '[ ..|.size? | numbers ] | add' // topic size  in bytes
